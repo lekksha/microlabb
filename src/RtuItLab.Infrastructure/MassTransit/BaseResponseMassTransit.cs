eee20e14802f9f47a033cee99c7586dbcb992588
DolceMeloy <159237077+DolceMeloy@users.noreply.github.com>
2026-04-15 15:48:56 +0300

    revert: restore ALL Infrastructure files byte-for-byte from commit 646d6f1f (batch 1/2)

--- src/RtuItLab.Infrastructure/MassTransit/BaseResponseMassTransit.cs
+++ src/RtuItLab.Infrastructure/MassTransit/BaseResponseMassTransit.cs
@@ -3,6 +3,5 @@ namespace RtuItLab.Infrastructure.MassTransit
     public class BaseResponseMassTransit
     {
         public bool IsSuccess { get; set; }
-        public string Error { get; set; }
     }
 }
--- src/RtuItLab.Infrastructure/MassTransit/Purchases/Requests/GetTransactionsRequest.cs
+++ src/RtuItLab.Infrastructure/MassTransit/Purchases/Requests/GetTransactionsRequest.cs
@@ -5,5 +5,8 @@ namespace RtuItLab.Infrastructure.MassTransit.Purchases.Requests
     public class GetTransactionsRequest
     {
         public User User { get; set; }
+        public int Page { get; set; }
+        public int Count { get; set; }
+        public bool IsShopCreate { get; set; }
     }
 }
